63a606284e34d3d957dbfe4e99ac1965c1bcce45
Alice Botella-Vigneault <alicebotella.v@hotmail.com>
2021-03-09 20:53:54 -0500

    ajout des menus avec les sessions 1-6 et des cours : cours du mardi 9mars

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Dépôt: https://github.com/Alibotella/exercice2
 
 Page gitHub: https://alibotella.github.io/exercice2/index.htm
+
+Enlever 3 commits pour le compte des 30 commits (bug avec readme)
--- themes/theme-4w4/category-cours.php
+++ themes/theme-4w4/category-cours.php
@@ -9,7 +9,7 @@
 
 get_header();
 ?>
-<!-- ARCHIVE.PHP --> /////////////////ARCHIVE.PHP
+<!-- ARCHIVE.PHP --> /////////////////CATEGORY-COURS.PHP
 	<main id="primary" class="site-main">
 
 		<?php if ( have_posts() ) : ?>
@@ -23,24 +23,19 @@ get_header();
 
 			<?php
 			/* Start the Loop */
+            $precedent = 0;
 			while ( have_posts() ) :
 				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
+                $titre = get_the_title();
+                $session = substr($titre, 4,1);
+                if ($precedent != $session) {
+                    echo '<p>Session : ' . $session . '</p>' ;
+                }
+                
+                echo '<p>' . $session . ' ' . $titre . '</p>' ; /* get_the_title fait apparaitre les titres -- <p> les met en liste*/
+                $precedent = $session;
 			endwhile;
-
-			the_posts_navigation();
-
 		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
 		endif;
 		?>
 
